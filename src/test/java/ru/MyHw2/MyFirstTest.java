087b8b79d64e8db6f7eeb0e23b35014efb653a49
Polina490 <6238polinka@gmail.com>
2022-08-08 22:42:08 +0300

    My second homework. git merge 1

---
+++ src/test/java/ru/MyHw2/MyFirstTest.java
@@ -0,0 +1,12 @@
+package ru.MyHw2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+
+    @Test
+    void myFirstTest(){
+        Assertions.assertTrue(5!=2);
+    }
+}
